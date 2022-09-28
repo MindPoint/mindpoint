9eec496606baca538fe3c4aaf99a6be9a60ef745
unknown <jvitor.sche@gmail.com>
2022-09-28 11:37:06 -0300

    Simplified the TextNode component

--- lib/widgets/molecule/nodes/text_node.dart
+++ lib/widgets/molecule/nodes/text_node.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:mindpoint/constants/font_families.dart';
-import 'package:mindpoint/constants/sizes.dart';
-import 'package:mindpoint/constants/units.dart';
-import 'package:mindpoint/constants/wheights.dart';
 import 'package:mindpoint/data/models/node.dart';
+import 'package:mindpoint/constants/font_families.dart';
 
 import '../../atoms/typography.dart';
 
@@ -19,12 +14,10 @@ class TextNode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CustomTypography(
-        overflow: TextOverflow.visible,
-        node.data,
-        fontFamily: FontFamilies.robotoSerif,
-      ),
+    return CustomTypography(
+      overflow: TextOverflow.visible,
+      node.data,
+      fontFamily: FontFamilies.robotoSerif,
     );
   }
 }
